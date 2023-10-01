dd0cefe8c91970c2ad64375f9c8a28007189c678
LeoX DEV <leoqonline@gmail.com>
2023-10-01 14:19:45 +0200

    added auto redirect

--- index.php
+++ index.php
@@ -10,4 +10,15 @@ if (isset($_REQUEST['url'])) {
     $req_url = '/' . $_REQUEST['url'];
 };
 
-echo($req_url);
+$req_url_split = explode('/', $req_url);
+
+
+switch ($req_url_split[1]) {
+    case 'spotify':
+        echo('spotify called');
+        break;
+    
+    default:
+        header('Location: https://github.com/Le0X8/additional-readme-stats');
+        break;
+};
