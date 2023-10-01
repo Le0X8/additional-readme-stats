9abb392a5f6d16c8a369165ad2953c8d3b167bd5
LeoX DEV <leoqonline@gmail.com>
2023-10-01 14:14:14 +0200

    added another testing page

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-phpinfo();
+
+$req_url = '/';
+
+if (isset($_SERVER['REQUEST_URI'])) {
+    $req_url = $_SERVER['REQUEST_URI'];
+};
+
+if (isset($_REQUEST['url'])) {
+    $req_url = '/' . $_REQUEST['url'];
+};
+
+echo($req_url);
